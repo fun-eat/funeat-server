31243f320b7f523776639f86b5077b5a22551937
Go-Jaecheol <gojaech@naver.com>
2024-01-14 18:01:00 +0900

    test: 상품 검색 결과 repository 테스트 추가

--- src/test/java/com/funeat/product/persistence/ProductRepositoryTest.java
+++ src/test/java/com/funeat/product/persistence/ProductRepositoryTest.java
@@ -24,6 +24,7 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
 
 @SuppressWarnings("NonAsciiCharacters")
 class ProductRepositoryTest extends RepositoryTest {
@@ -128,7 +129,7 @@ class ProductRepositoryTest extends RepositoryTest {
     }
 
     @Nested
-    class findAllWithReviewCountByNameContaining_성공_테스트 {
+    class findAllWithReviewCountByNameContainingFirst_성공_테스트 {
 
         @Test
         void 상품명에_검색어가_포함된_상품들과_리뷰_수를_조회한다() {
@@ -149,14 +150,49 @@ class ProductRepositoryTest extends RepositoryTest {
             final var review2_1 = 리뷰_이미지test3_평점3점_재구매O_생성(member1, product2, 0L);
             복수_리뷰_저장(review1_1, review1_2, review2_1);
 
-            final var page = 페이지요청_기본_생성(0, 10);
+            final var expectedDto1 = new ProductReviewCountDto(product1, 2L);
+            final var expectedDto2 = new ProductReviewCountDto(product2, 1L);
+            final var expected = List.of(expectedDto2, expectedDto1);
+
+            // when
+            final var actual = productRepository.findAllWithReviewCountByNameContainingFirst("망고", PageRequest.of(0, 2));
+
+            // then
+            assertThat(actual).usingRecursiveComparison()
+                    .isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    class findAllWithReviewCountByNameContaining_성공_테스트 {
+
+        @Test
+        void 상품명에_검색어가_포함된_상품들과_리뷰_수를_조회한다() {
+            // given
+            final var category = 카테고리_간편식사_생성();
+            단일_카테고리_저장(category);
+
+            final var product1 = 상품_애플망고_가격3000원_평점5점_생성(category);
+            final var product2 = 상품_망고빙수_가격5000원_평점4점_생성(category);
+            final var product3 = 상품_망고빙수_가격5000원_평점4점_생성(category);
+            final var product4 = 상품_망고빙수_가격5000원_평점4점_생성(category);
+            복수_상품_저장(product1, product2, product3, product4);
+
+            final var member1 = 멤버_멤버1_생성();
+            final var member2 = 멤버_멤버2_생성();
+            복수_멤버_저장(member1, member2);
+
+            final var review1_1 = 리뷰_이미지test1_평점1점_재구매X_생성(member1, product1, 0L);
+            final var review1_2 = 리뷰_이미지test5_평점5점_재구매O_생성(member2, product1, 0L);
+            final var review2_1 = 리뷰_이미지test3_평점3점_재구매O_생성(member1, product2, 0L);
+            복수_리뷰_저장(review1_1, review1_2, review2_1);
 
             final var expectedDto1 = new ProductReviewCountDto(product1, 2L);
             final var expectedDto2 = new ProductReviewCountDto(product2, 1L);
             final var expected = List.of(expectedDto2, expectedDto1);
 
             // when
-            final var actual = productRepository.findAllWithReviewCountByNameContaining("망고", page).getContent();
+            final var actual = productRepository.findAllWithReviewCountByNameContaining("망고", 3L, PageRequest.of(0, 4));
 
             // then
             assertThat(actual).usingRecursiveComparison()
