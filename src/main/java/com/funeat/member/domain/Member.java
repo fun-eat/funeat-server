9a1520ccf2c639a80afe278a2812c65b9e7e9f63
우가 <dnr3655@naver.com>
2024-01-15 12:24:33 +0900

    chore: 2.7.1 -> 3.2.1 springboot 마이그레이션 (#8)
    * chore: 2.7.1 -> 3.2.1 springboot 마이그레이션
    
    * refactor: 리뷰 반영
    
    * chore: ci-cd jdk 버전 21로 업데이트
    
    * chore: test.yml jdk 버전 21로 업데이트

--- src/main/java/com/funeat/member/domain/Member.java
+++ src/main/java/com/funeat/member/domain/Member.java
@@ -8,11 +8,11 @@ import com.funeat.member.exception.MemberException.MemberUpdateException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import org.springframework.util.StringUtils;
 
 @Entity
