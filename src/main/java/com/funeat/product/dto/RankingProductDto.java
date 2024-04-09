87cbbdefa133992bcc2c6bc2bb092dedacc04efa
Dabeen Jeong <hello70825@gmail.com>
2024-04-09 12:11:01 +0900

    refactor: 상품 랭킹 DTO에 상품 가격 정보 추가 (#36)

--- src/main/java/com/funeat/product/dto/RankingProductDto.java
+++ src/main/java/com/funeat/product/dto/RankingProductDto.java
@@ -6,19 +6,22 @@ public class RankingProductDto {
 
     private final Long id;
     private final String name;
+    private final Long price;
     private final String image;
     private final String categoryType;
 
-    public RankingProductDto(final Long id, final String name, final String image, final String categoryType) {
+    public RankingProductDto(final Long id, final String name, final Long price,
+                             final String image, final String categoryType) {
         this.id = id;
         this.name = name;
+        this.price = price;
         this.image = image;
         this.categoryType = categoryType;
     }
 
     public static RankingProductDto toDto(final Product product) {
-        return new RankingProductDto(product.getId(), product.getName(), product.getImage(),
-                product.getCategory().getType().getName());
+        return new RankingProductDto(product.getId(), product.getName(), product.getPrice(),
+                product.getImage(), product.getCategory().getType().getName());
     }
 
     public Long getId() {
@@ -27,6 +30,10 @@ public class RankingProductDto {
 
     public String getName() {
         return name;
+    }
+
+    public Long getPrice() {
+        return price;
     }
 
     public String getImage() {
