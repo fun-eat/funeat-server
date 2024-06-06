6e1701573b9d1206f5037ebca4da99d109e2d518
wugawuga <dnr3655@naver.com>
2024-06-06 10:41:35 +0900

    refactor: 쿼리 수정 "\s" 제거

--- src/main/java/com/funeat/product/persistence/ProductRepositoryImpl.java
+++ src/main/java/com/funeat/product/persistence/ProductRepositoryImpl.java
@@ -19,21 +19,21 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
     @Override
     public List<Product> searchProductsByTopTagsFirst(final Long tagId, final Pageable pageable) {
         final String jpql = """
-                SELECT DISTINCT p\s
+                SELECT DISTINCT p
                 FROM Product p
                 WHERE p.id IN (
-                    SELECT p2.id\s
-                    FROM Product p2\s
-                    JOIN p2.reviews r2\s
-                    JOIN r2.reviewTags rt2\s
-                    WHERE rt2.tag.id = :tagId\s
+                    SELECT p2.id
+                    FROM Product p2
+                    JOIN p2.reviews r2
+                    JOIN r2.reviewTags rt2
+                    WHERE rt2.tag.id = :tagId
                       AND rt2.tag.id IN (
-                          SELECT rt3.tag.id\s
-                          FROM Review r3\s
-                          JOIN r3.reviewTags rt3\s
-                          WHERE r3.product.id = p2.id\s
-                          GROUP BY rt3.tag.id\s
-                          ORDER BY COUNT(rt3.tag.id) DESC\s
+                          SELECT rt3.tag.id
+                          FROM Review r3
+                          JOIN r3.reviewTags rt3
+                          WHERE r3.product.id = p2.id
+                          GROUP BY rt3.tag.id
+                          ORDER BY COUNT(rt3.tag.id) DESC
                           LIMIT 3
                       )
                 )
@@ -51,27 +51,27 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
     @Override
     public List<Product> searchProductsByTopTags(final Long tagId, final Long lastProductId, final Pageable pageable) {
         final String jpql = """
-                SELECT DISTINCT p\s
-                FROM Product p\s
-                WHERE p.id < :lastProductId\s
+                SELECT DISTINCT p
+                FROM Product p
+                WHERE p.id < :lastProductId
                 AND p.id IN (
-                    SELECT p2.id\s
-                    FROM Product p2\s
-                    JOIN p2.reviews r2\s
-                    JOIN r2.reviewTags rt2\s
-                    WHERE rt2.tag.id = :tagId\s
+                    SELECT p2.id
+                    FROM Product p2
+                    JOIN p2.reviews r2
+                    JOIN r2.reviewTags rt2
+                    WHERE rt2.tag.id = :tagId
                     AND rt2.tag.id IN (
-                        SELECT rt3.tag.id\s
-                        FROM Review r3\s
-                        JOIN r3.reviewTags rt3\s
-                        WHERE r3.product.id = p2.id\s
-                        GROUP BY rt3.tag.id\s
-                        ORDER BY COUNT(rt3.tag.id) DESC\s
+                        SELECT rt3.tag.id
+                        FROM Review r3
+                        JOIN r3.reviewTags rt3
+                        WHERE r3.product.id = p2.id
+                        GROUP BY rt3.tag.id
+                        ORDER BY COUNT(rt3.tag.id) DESC
                         LIMIT 3
                     )
-                    GROUP BY p2.id\s
-                    HAVING COUNT(DISTINCT rt2.tag.id) <= 3\s
-                )\s
+                    GROUP BY p2.id
+                    HAVING COUNT(DISTINCT rt2.tag.id) <= 3
+                )
                 ORDER BY p.id DESC
                 """;
 
